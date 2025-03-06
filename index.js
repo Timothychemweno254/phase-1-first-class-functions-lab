cd2c38a23942d3a6b2b2f063830063704da91206
timothychemweno254 <timothy.kiprop@student.moringaschool.com>
2025-03-06 12:22:52 +0300

    last js lab

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Code your solution in this file!
+// returnFirstTwoDrivers
+function returnFirstTwoDrivers(drivers) {
+    return drivers.slice(0, 2);
+  }
+  
+  // returnLastTwoDrivers
+  function returnLastTwoDrivers(drivers) {
+    return drivers.slice(-2);
+  }
+  
+  // selectingDrivers
+  const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+  
+  // createFareMultiplier
+  function createFareMultiplier(multiplier) {
+    return function (fare) {
+      return fare * multiplier;
+    };
+  }
+  
+  // fareDoubler
+  const fareDoubler = createFareMultiplier(2);
+  
+  // fareTripler
+  const fareTripler = createFareMultiplier(3);
+  
+  // selectDifferentDrivers
+  function selectDifferentDrivers(drivers, driverFunction) {
+    return driverFunction(drivers);
+  }
